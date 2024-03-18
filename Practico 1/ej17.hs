6f31f33a1f425b5d3719310a0f8cf9d12a8b41de
Lichi <lichimarquez04@gmail.com>
2024-03-18 17:15:10 -0300

    Ejercicio 17 finalizado

---
+++ Practico 1/ej17.hs
@@ -0,0 +1,9 @@
+main :: IO ()
+main = do
+    -- CONSIGNA: Dar el tipo de las siguientes funciones:
+    putStrLn"a) g y = 8 ∗ y"
+    putStrLn"g : Num -> Num -> Num"
+    putStrLn"b) h z w = z + w"
+    putStrLn"h : Num -> Num -> Num"
+    putStrLn"c) j x = x ⩽ 0"
+    putStrLn"j : Num -> Num -> Bool"

1115f8994e946c4ac5ba56e3c083b436fe568ead
Lichi <lichimarquez04@gmail.com>
2024-03-18 16:13:41 -0300

    Ejercicio 6 finalizado

---
+++ Practico 1/ej6.hs
@@ -0,0 +1,6 @@
+-- Declaramos main, que es la función principal en Haskell
+main :: IO ()
+main = do
+    -- CONSIGNA: Construi una formula de acuerdo con las reglas precedentes. ¿Como podrias justificar que esta bien construida?
+    putStrLn "El factorial de 5 es igual a:"
+    print (5 * 4 * 3 * 2)

966f6598f8d4a287bfec4b5201b76153a0ab1fa9
Lichi <lichimarquez04@gmail.com>
2024-03-18 15:31:35 -0300

    Practico 1 inicializado

---
+++ Practico 1/ej2.hs
@@ -0,0 +1,5 @@
+main = do
+    let var1= 2
+    let var2= 1
+    print(var1 + var2)
+    print "El error que nos levanta Haskell es que la sintaxis no es correcta, ya que la suma necesita de dos operadores [a+b] para así generar el resultado y escribirlo en la consola [c]"

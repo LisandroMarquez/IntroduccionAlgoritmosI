a08910aac8d7b68e40180dd785178f5e7afd0d62
Lichi <lichimarquez04@gmail.com>
2024-03-18 15:42:07 -0300

    Ejercicio 2 finalizado

--- Practico 1/ej2.hs
+++ Practico 1/ej2.hs
@@ -1,5 +1,11 @@
+-- Declaramos main, que es la función principal en Haskell
+main :: IO ()
 main = do
-    let var1= 2
-    let var2= 1
-    print(var1 + var2)
-    print "El error que nos levanta Haskell es que la sintaxis no es correcta, ya que la suma necesita de dos operadores [a+b] para así generar el resultado y escribirlo en la consola [c]"
+    -- Asignamos valores a las variables var1 y var2
+    let var1 = 2 :: Int
+        var2 = 1 :: Int
+    -- Imprimimos las sumas solicitadas
+    print (var1 + var2)
+    {- print (var1 +) Esta linea no tiene sentido
+    Imprimimos un mensaje en la consola -}
+    putStrLn "El error que nos levanta Haskell al escribir 'print (var1 +)' es que la suma necesita de dos operadores [a+b] para así generar el resultado y escribirlo en la consola [c]"

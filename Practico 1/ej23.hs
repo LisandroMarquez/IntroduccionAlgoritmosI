ec013f012c85d8e27c957c6259d7fb237a45bd73
Lichi <lichimarquez04@gmail.com>
2024-03-18 20:29:24 -0300

    Ejercicio 23 fixed again

--- Practico 1/ej23.hs
+++ Practico 1/ej23.hs
@@ -2,13 +2,13 @@ main :: IO ()
 main = do
     -- CONSIGNA: Definir la funcion fahrToCelsius : Num → Num, la inversa de la anterior. Para realizar la conversion hay que primero restar 32 y despues dividir por 1.8.
     let fahrToCelsius x = (x - 32) / 1.8
-    putStrLn"32° Celcius es equivalente en Fahrenheit a"
+    putStrLn"32° Fahrenheit es equivalente en Celsius a"
     print(fahrToCelsius 32)
-    putStrLn"465.8° Celcius es equivalente en Fahrenheit a"
+    putStrLn"465.8° Fahrenheit es equivalente en Celsius a"
     print(fahrToCelsius 465.8)
-    putStrLn"5° Celcius es equivalente en Fahrenheit a"
+    putStrLn"5° Fahrenheit es equivalente en Celsius a"
     print(fahrToCelsius 5)
-    putStrLn"96.8° Celcius es equivalente en Fahrenheit a"
+    putStrLn"96.8° Fahrenheit es equivalente en Celsius a"
     print(fahrToCelsius 96.8)
-    putStrLn"80.6° Celcius es equivalente en Fahrenheit a"
+    putStrLn"80.6° Fahrenheit es equivalente en Celsius a"
     print(fahrToCelsius 80.6)
